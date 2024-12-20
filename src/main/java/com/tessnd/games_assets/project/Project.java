41ac94de80ba63e1d7d69eb07a44f7fd2caaa034
TessND <114303080+TessND@users.noreply.github.com>
2024-12-20 21:31:00 +0400

    project entity added

--- src/main/java/com/tessnd/games_assets/project/Project.java
+++ src/main/java/com/tessnd/games_assets/project/Project.java
@@ -2,13 +2,17 @@ package com.tessnd.games_assets.project;
 
 import com.tessnd.games_assets.user.User;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
 import lombok.Data;
 
 @Entity
@@ -25,7 +29,13 @@ public class Project {
     @Column(columnDefinition = "TEXT")
     private String description;
 
-    @ManyToOne
-    
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_projects",
+        joinColumns = @JoinColumn(name = "project_id"),
+        inverseJoinColumns = @JoinColumn(name = "user_id"))
     private User user;
+
+    @Column(columnDefinition = "TEXT")
+    private String link;
+
 }
